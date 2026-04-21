672ba787b9da28fe4cbe3088e577896437a2f954
tylorg811-byte <tylorg811@gmail.com>
2026-04-20 23:51:56 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,12 +2,12 @@ const elements = document.querySelectorAll(".fade-in");
 
 const observer = new IntersectionObserver((entries) => {
   entries.forEach(entry => {
-    if (entry.isIntersecting) {
+    if(entry.isIntersecting){
       entry.target.classList.add("show");
     }
   });
-}, {
-  threshold: 0.15
+},{
+  threshold:0.15
 });
 
 elements.forEach(el => observer.observe(el));
